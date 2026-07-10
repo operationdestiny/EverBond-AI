35d6f24e4c24255518d3e86539b2f357c2380ca1
Dawson Smith <1sallyspace@gmail.com>
2026-07-10 10:43:02 -0400

    Remove incorrect patch folder

--- everbond-option-a-runtime-patch/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { HomeCompanionBrowser } from "@/components/character/HomeCompanionBrowser";
-import { AppShell } from "@/components/layout/AppShell";
-import { getCharactersFromSupabase } from "@/lib/characters-db";
-
-export default async function HomePage() {
-  const characters = await getCharactersFromSupabase(100, 0, "everbond-girls");
-  return <AppShell><HomeCompanionBrowser characters={characters} /></AppShell>;
-}
